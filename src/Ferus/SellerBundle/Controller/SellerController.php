f21e5cbc0156823357ae7e1018f30ed4c8032733
nick-keller <solix@hotmail.fr>
2014-08-22 14:08:07 +0200

    Finished expiration date for cash register

--- src/Ferus/SellerBundle/Controller/SellerController.php
+++ src/Ferus/SellerBundle/Controller/SellerController.php
@@ -5,6 +5,7 @@ namespace Ferus\SellerBundle\Controller;
 
 use Braincrafted\Bundle\BootstrapBundle\Session\FlashMessage;
 use Doctrine\ORM\EntityManager;
+use Ferus\SellerBundle\Entity\Seller;
 use Ferus\SellerBundle\Form\SellType;
 use Ferus\TransactionBundle\Entity\Transaction;
 use Ferus\UserBundle\Entity\User;
@@ -37,6 +38,12 @@ class SellerController extends Controller
     {
         if($user->getAccount()->getSeller() == null) return $this->createNotFoundException();
 
+        if($user->getAccount()->getSeller()->getCashRegisterExpiresAt() < new \DateTime)
+            return array(
+                'seller' => $user->getAccount()->getSeller(),
+                'expired' => true,
+            );
+
         $transaction = new Transaction;
         $transaction->setReceiver($user->getAccount());
         $transaction->setCause('Stand '.$user->getAccount()->getSeller());
@@ -50,6 +57,31 @@ class SellerController extends Controller
         return array(
             'seller' => $user->getAccount()->getSeller(),
             'form' => $form->createView(),
+        );
+    }
+
+    /**
+     * @Template
+     */
+    public function dashboardAction(Request $request)
+    {
+        $seller = $this->getUser()->getAccount()->getSeller();
+        if($seller == null) return $this->createNotFoundException();
+
+        if(preg_match('/^((15|30) min|[1-3] hours?)$/', $request->query->get('add_time', ''))){
+            if($seller->getCashRegisterExpiresAt() > new \DateTime)
+                $newDate = $seller->getCashRegisterExpiresAt()->modify('+'.$request->query->get('add_time'));
+            else
+                $newDate = new \DateTime('+'.$request->query->get('add_time'));
+
+            $seller->setCashRegisterExpiresAt($newDate);
+
+            $this->em->persist($seller);
+            $this->em->flush();
+        }
+
+        return array(
+            'seller' => $seller,
         );
     }
 } 
