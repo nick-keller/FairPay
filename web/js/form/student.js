f2aaa9d998062769469c48b330456e9acfd05704
nick-keller <solix@hotmail.fr>
2014-09-03 15:55:05 +0200

    Improved student search

--- web/js/form/student.js
+++ web/js/form/student.js
@@ -7,16 +7,30 @@ $(function(){
                     var $this = $(this);
                     var $helpTxt = $this.parents('.input-group').next();
 
-                    $.get($this.data('api').replace('-query-', $this.val()), function(data){
-                        $this.val(data.id);
-                        $this.blur();
-                        $helpTxt.html('<b>' + data.first_name + ' ' + data.last_name + '</b>');
+                    if($this.data('searching') == true){
+                        $this.data('waiting', true);
+                        return;
+                    }
+                    $this.data('searching', true);
 
-                        if(data.is_contributor)
-                            $helpTxt.append(' cotisant');
-                        else
-                            $helpTxt.append(' non-cotisant');
-                    });
+                    setTimeout(function(){
+                        $.get($this.data('api').replace('-query-', $this.val()), function(data){
+                            $this.data('searching', false);
+                            if($this.data('waiting')){
+                                $this.data('waiting', false);
+                                $this.keyup();
+                            }
+
+                            $this.val(data.id);
+                            $this.blur();
+                            $helpTxt.html('<b>' + data.first_name + ' ' + data.last_name + '</b>');
+
+                            if(data.is_contributor)
+                                $helpTxt.append(' cotisant');
+                            else
+                                $helpTxt.append(' non-cotisant');
+                        });
+                    }, 100);
                 })
                 .keyup(function(){
                     $(this).change();
