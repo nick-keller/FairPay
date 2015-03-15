c1f428befe2474bd4a71da8ff84b81cee510d84f
nick-keller <solix@hotmail.fr>
2015-03-15 15:18:09 +0100

    Created main entities and admin

---
+++ src/Ferus/FCFSBundle/Form/EventType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Ferus\FCFSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class EventType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', 'text', array(
+                'label' => 'Nom',
+            ))
+            ->add('maxPeople', 'integer', array(
+                'label' => 'Nombre max de personnes',
+            ))
+            ->add('actions', 'form_actions', [
+                'buttons' => array(
+                    'save' => [
+                        'type' => 'submit',
+                        'options' => [
+                            'label' => 'Enregistrer',
+                        ]
+                    ],
+                )
+            ])
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Ferus\FCFSBundle\Entity\Event'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ferus_fcfsbundle_event';
+    }
+}
